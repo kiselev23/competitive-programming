cfe05a594a8c6798122fa9c28dab8660b9fc4a51
kiselev23 <86372922+kiselev23@users.noreply.github.com>
2025-10-08 22:38:53 +0300

    Update Template.cpp

--- codeforces/Template.cpp
+++ codeforces/Template.cpp
@@ -1,1 +1,53 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <random>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <ctime>
+#include <climits>
+#include <limits>
 
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using str = string;
+using chr = char;
+
+#define all(x) x.begin(),x.end()
+#define range(i, n) for (int i=0;i<int(n);++i)
+#define range(i, s, f) for (int i=(s);i<int(f);++i)
+#define rrange(i,n) for (int i=(n)-1;i>=0;--i)
+#define mp make_pair
+#define CF(x) int x;cin >> x;while(x--)
+#define md(x) if (x>=mod) x%=mod
+
+const ll MOD = 1e9 + 5;
+const double INF = 1e18;
+
+template<typename T>istream& operator >>(istream& in, vector<T>& a) { range(i, a.size()) in >> a[i]; return in; }
+template<typename T>ostream& operator <<(ostream& out, const vector<T>& a) { range(i, a.size()) out << a[i] << ' '; return out; }
+
+template<typename T1, typename T2>istream& operator >>(istream& in, pair<T1, T2>& p) { in >> p.first >> p.second; return in; }
+template<typename T1, typename T2>ostream& operator <<(ostream& out, const pair<T1, T2>& p) { out << p.first << ' ' << p.second; return out; }
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+
+
+  
+	return 0;
+}
